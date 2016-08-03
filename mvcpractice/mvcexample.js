feba555968a41556c561411177de0645795e3c3e
dillon-mckee <dillon.mckee@gmail.com>
2016-08-03 12:02:21 -0400

    added example files

---
+++ mvcpractice/mvcexample.js
@@ -0,0 +1,62 @@
+// defining Model as a function
+// setting this.text to empty string
+// setting this.OnChange to null
+var Model = function() {
+  this.text = "";
+  this.onChange = null;
+};
+
+// Adding setText method which converts everything to uppercase
+// eventListener listening for change on DOM
+Model.prototype.setText = function(value) {
+  this.text = value.toUpperCase();
+  if (this.onChange) {
+    this.onChange(this.text);
+  }
+};
+
+// View function taking two parameters: a selector for the input
+// and an optional initial value.
+// setting this.OnChange to null
+// eventListener for change of input
+
+ var View = function(elementSelector, initialValue) {
+    this.element = $(elementSelector);
+    this.setValue(initialValue || '');
+    this.onChange = null;
+
+    this.element.on('input', this.onInput.bind(this));
+};
+
+// Adding onInput method that listens for input event and calls onChange
+
+
+View.prototype.onInput = function() {
+    var value = this.element.val();
+    if (this.onChange) {
+        this.onChange(value);
+    }
+};
+
+// Defining setValue method to retrieve value of text
+
+View.prototype.setValue = function(text) {
+    this.element.val(text);
+};
+
+// controller links  the callbacks from the model and the view
+// to their corresponding actions using .bind.
+// When the user enters something in the view, the model's text
+// is changed. And when the model's text is changed, the view
+// has its value updated.
+
+var Controller = function(model, view) {
+  view.onChange = model.setText.bind(model);
+  model.onChange = view.setValue.bind(view);
+};
+
+//document.addEventListener('DOMContentLoaded', function() {
+    var model = new Model();
+    var view = new View('.uppercase');
+    var controller = new Controller(model, view);
+//});
