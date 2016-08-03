6a995d1b809cf6beabfc0fbe340815fe77d2ea8c
justinmarkhamlawrence <justinmarkhamlawrence@gmail.com>
2016-08-03 14:40:07 -0400

    more view

--- newindex.js
+++ newindex.js
@@ -89,50 +89,58 @@ console.log(myQuiz.score);
 /// questions, answers, score, currentquestion, useranswers
 var quizView = function(questionsSelector) {
   this.questionsPageElement = $(questionsSelector);
-  this.questionCurrentElement = $('.question-current');
-  this.questionsTotalElement = $('.questions-total');
-  this.questionElement = $('.question');
-  this.answersElement = $('.answers');
-  this.resultsPageElement = $('.results-page');
-  this.scoreElement = $('.score');
-  this.restartButtonElement = $('.restart-button');
+  this.questionCurrentElement = $(questionCurrent);
+  this.questionsTotalElement = $(questionsTotal);
+  this.questionElement = $(question);
+  this.answersElement = $(answers);
+  this.resultsPageElement = $(resultsPage);
+  this.scoreElement = $(score);
+  this.restartButtonElement = $(restartButton);
 }
 quizView.prototype.showResults = function() {
     questionsPageElement.hide();
     resultsPageElement.show();
 };
-quizView.prototype.resetScore = function() {
-    scoreElement.text(0);
-};
-quizView.prototype.increaseScore = function() {
-    var score = parseInt(scoreElement.text(), 10);
-    scoreElement.text(score + 1);
-};
-quizView.prototype.setQuestion = function(currentquestion) {
-    var question = questions[currentquestion];
-    questionCurrentElement.text(currentquestion);
-    questionElement.text(question.text);
-    answersElement.empty();
-    for (var i=0; i<question.answers.length; i++) {
-        var answer = question.answers[i];
-        answersElement.append('<li><button type="button">' + answer + '</button></li>');
-    }
-};
-quizView.prototype.answersElement.on('click', 'button', function() {
-    var choice = $(this).parent().index();
-    var currentquestion = parseInt(questionCurrentElement.text(), 10);
-    var question = questions[currentquestion];
-    if (question.correct === choice) {
-        increaseScore();
-    }
 
-    if (currentquestion + 1 < questions.length) {
-        setQuestion(currentquestion + 1);
-    }
-    else {
-        showResults();
-    }
-});
+quizView.prototype.displayQuestion = function() {
+  //on page load, display current question
+  this.questionElement.append(this.questions.text[this.currentquestion]);
+  console.log(this.questions.text[this.currentquestion]);
+};
+
+var myQuizView = new quizView('.questions-page',...);
+// quizView.prototype.resetScore = function() {
+//     scoreElement.text(0);
+// };
+// quizView.prototype.increaseScore = function() {
+//     var score = parseInt(scoreElement.text(), 10);
+//     scoreElement.text(score + 1);
+// };
+// quizView.prototype.setQuestion = function(currentquestion) {
+//     var question = questions[currentquestion];
+//     questionCurrentElement.text(currentquestion);
+//     questionElement.text(question.text);
+//     answersElement.empty();
+//     for (var i=0; i<question.answers.length; i++) {
+//         var answer = question.answers[i];
+//         answersElement.append('<li><button type="button">' + answer + '</button></li>');
+//     }
+// };
+// quizView.prototype.answersElement.on('click', 'button', function() {
+//     var choice = $(this).parent().index();
+//     var currentquestion = parseInt(questionCurrentElement.text(), 10);
+//     var question = questions[currentquestion];
+//     if (question.correct === choice) {
+//         increaseScore();
+//     }
+
+//     if (currentquestion + 1 < questions.length) {
+//         setQuestion(currentquestion + 1);
+//     }
+//     else {
+//         showResults();
+//     }
+// });
 //show questions to the DOM
 //accept answers
 //show question number
