687fa03ad99be9f3a3658c88955b136490dd1f6b
justinmarkhamlawrence <justinmarkhamlawrence@gmail.com>
2016-08-03 12:01:07 -0400

    started on view

--- newindex.js
+++ newindex.js
@@ -61,7 +61,7 @@ quizModel.prototype.answer = function(answer) {
 
 quizModel.prototype.checkAnswers = function() {
   for (var i = 0; i < this.useranswers.length; i++) {
-    if (this.useranswers[0] == this.questions[0].correct) {
+    if (this.useranswers[i] == this.questions[i].correct) {
       this.score ++ ;
     }  
   };
@@ -87,3 +87,54 @@ console.log(myQuiz.score);
 
 /// What does View want from Model?
 /// questions, answers, score, currentquestion, useranswers
+var quizView = function(questionsSelector) {
+  this.questionsPageElement = $(questionsSelector);
+  this.questionCurrentElement = $('.question-current');
+  this.questionsTotalElement = $('.questions-total');
+  this.questionElement = $('.question');
+  this.answersElement = $('.answers');
+  this.resultsPageElement = $('.results-page');
+  this.scoreElement = $('.score');
+  this.restartButtonElement = $('.restart-button');
+}
+quizView.prototype.showResults = function() {
+    questionsPageElement.hide();
+    resultsPageElement.show();
+};
+quizView.prototype.resetScore = function() {
+    scoreElement.text(0);
+};
+quizView.prototype.increaseScore = function() {
+    var score = parseInt(scoreElement.text(), 10);
+    scoreElement.text(score + 1);
+};
+quizView.prototype.setQuestion = function(currentquestion) {
+    var question = questions[currentquestion];
+    questionCurrentElement.text(currentquestion);
+    questionElement.text(question.text);
+    answersElement.empty();
+    for (var i=0; i<question.answers.length; i++) {
+        var answer = question.answers[i];
+        answersElement.append('<li><button type="button">' + answer + '</button></li>');
+    }
+};
+quizView.prototype.answersElement.on('click', 'button', function() {
+    var choice = $(this).parent().index();
+    var currentquestion = parseInt(questionCurrentElement.text(), 10);
+    var question = questions[currentquestion];
+    if (question.correct === choice) {
+        increaseScore();
+    }
+
+    if (currentquestion + 1 < questions.length) {
+        setQuestion(currentquestion + 1);
+    }
+    else {
+        showResults();
+    }
+});
+//show questions to the DOM
+//accept answers
+//show question number
+//change questions 
+//show report card
